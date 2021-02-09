3b294aa5b744275cfe4a20337eab84e22e024df1
mingeon-kim <65947346+mingeon-kim@users.noreply.github.com>
2021-02-09 22:10:48 +0900

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button,Progress } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>안녕하세요</p>
+      <Button primary> 으히히히</Button>
+      <progress percent = {66}/>
     </div>
   );
 }
